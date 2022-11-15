420e61527c5a6d85d9d25265e30d1709ae3f5bd4
daivamahdyatha16 <daivamahdyatha16@gmail.com>
2022-11-16 00:50:02 +0700

    +update

--- src/main/java/tugas1/tugas1/BeanConfiguration.java
+++ src/main/java/tugas1/tugas1/BeanConfiguration.java
@@ -3,9 +3,15 @@ package tugas1.tugas1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import tugas1.tugas1.data.Hewan;
 import tugas1.tugas1.data.Manusia;
+import tugas1.tugas1.data.Tumbuhan;
 import tugas1.tugas1.data.depedency.Tangan;
 import tugas1.tugas1.data.depedency.Kaki;
+import tugas1.tugas1.data.depedency.Mulut;
+import tugas1.tugas1.data.depedency.Ekor;
+import tugas1.tugas1.data.depedency.Akar;
+import tugas1.tugas1.data.depedency.Daun;
 
 @Configuration
 public class BeanConfiguration {
@@ -25,4 +31,33 @@ public class BeanConfiguration {
         return new Manusia(tangan, kaki);
     }
 
+    @Bean
+    public Mulut mulut() {
+        return new Mulut();
+    }
+
+    @Bean
+    public Ekor ekor() {
+        return new Ekor();
+    }
+
+    @Bean
+    public Hewan hewan(Mulut mulut, Ekor ekor) {
+        return new Hewan(ekor, mulut);
+    }
+
+    @Bean
+    public Daun daun() {
+        return new Daun();
+    }
+
+    @Bean
+    public Akar akar() {
+        return new Akar();
+    }
+
+    @Bean
+    public Tumbuhan tumbuhan(Daun daun, Akar akar) {
+        return new Tumbuhan(daun, akar);
+    }
 }
